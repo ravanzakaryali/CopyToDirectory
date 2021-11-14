f6d0ea8bc9131bb300f0f52562e3541ca293cf52
revanzli <revanzekeryeli2002@gmail.com>
2021-11-14 20:29:03 +0400

    added all folder and file

---
+++ CopyDirectory/CopyDirectory/Models/CopyToDirectory.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace CopyDirectory.Models
+{
+    public class CopyToDirectory
+    {
+        public static void Copy(string sourceDir, string targetDir)
+        {
+            DirectoryInfo dirSource = new DirectoryInfo(sourceDir);
+            DirectoryInfo dirTarget = new DirectoryInfo(targetDir);
+
+            CopyAll(dirSource, dirTarget);
+        }
+        public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+            foreach (FileInfo fi in source.GetFiles())
+            {
+                fi.CopyTo(Path.Combine(target.FullName, fi.Name), true);
+            }
+
+            foreach (DirectoryInfo dirSourceSubDir in source.GetDirectories())
+            {
+                DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(dirSourceSubDir.Name);
+                CopyAll(dirSourceSubDir, nextTargetSubDir);
+            }
+        }
+    }
+}
---
+++ CopyDirectory/CopyDirectory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using CopyDirectory.Models;
+
+namespace CopyDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the link of the source folder: ");
+            string inputSource = Console.ReadLine();
+            var MovePath = new DirectoryInfo(inputSource);
+            Console.Write("Please enter the link to the location you want to copy: ");
+            string inputTarget = Console.ReadLine();
+            var CurrentDirectory = new DirectoryInfo(inputTarget);
+            CopyToDirectory.CopyAll(MovePath, CurrentDirectory);
+        }
+    }
+}
