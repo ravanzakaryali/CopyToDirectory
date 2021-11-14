ba9287d24c087996cfbecfee4e400b8bc654009c
revanzli <revanzekeryeli2002@gmail.com>
2021-11-14 23:53:18 +0400

    added summery comment

--- CopyDirectory/CopyDirectory/Models/CopyToDirectory.cs
+++ CopyDirectory/CopyDirectory/Models/CopyToDirectory.cs
@@ -4,6 +4,11 @@ namespace CopyDirectory.Models
 {
     public class CopyToDirectory
     {
+        /// <summary>
+        /// String converts incoming links to a diractory type.
+        /// </summary>
+        /// <param name="sourceDir">Link of the source folder</param>
+        /// <param name="targetDir">Link to the location you want to copy</param>
         public static void Copy(string sourceDir, string targetDir)
         {
             DirectoryInfo dirSource = new DirectoryInfo(sourceDir);
@@ -11,8 +16,14 @@ namespace CopyDirectory.Models
 
             CopyAll(dirSource, dirTarget);
         }
+        /// <summary>
+        /// It can be used to upload all the folders and files in one folder to another file.
+        /// </summary>
+        /// <param name="source">Link of the source folder </param>
+        /// <param name="target">Link to the location you want to copy</param>
         public static void CopyAll(DirectoryInfo source, DirectoryInfo target)
         {
+
             Directory.CreateDirectory(target.FullName);
 
             foreach (FileInfo fi in source.GetFiles())
